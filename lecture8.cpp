cc6efc32c4070dfa8ebe8c5f2171967f1b6cbc81
Manish Ranjan <ranjanmanish516@gmail.com>
2026-01-13 17:20:03 +0530

    Day X: short learning summary

--- lecture8.cpp
+++ lecture8.cpp
@@ -1,32 +1,42 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     // Size of the array
+//     int n = 5;
+
+//     // Array declaration and initialization
+//     int arr[5] = {1, 2, 3, 4, 5};
+
+//     // Loop for starting index of subarray
+//     for(int st = 0; st < n; st++){
+
+//         // Loop for ending index of subarray
+//         for(int end = st; end < n; end++){
+
+//             // Loop to print elements from st to end
+//             for(int i = st; i <= end; i++){
+//                 cout << arr[i];
+//             }
+
+//             // Space after each subarray
+//             cout << " ";
+//         }
+
+//         // New line after finishing one starting index
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 
 int main(){
-
-    // Size of the array
-    int n = 5;
-
-    // Array declaration and initialization
-    int arr[5] = {1, 2, 3, 4, 5};
-
-    // Loop for starting index of subarray
-    for(int st = 0; st < n; st++){
-
-        // Loop for ending index of subarray
-        for(int end = st; end < n; end++){
-
-            // Loop to print elements from st to end
-            for(int i = st; i <= end; i++){
-                cout << arr[i];
-            }
-
-            // Space after each subarray
-            cout << " ";
-        }
-
-        // New line after finishing one starting index
-        cout << endl;
-    }
 
     return 0;
 }
