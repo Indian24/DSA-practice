be840290f8434e3ed028229141b5202568fffae5
Manish Ranjan <ranjanmanish516@gmail.com>
2026-02-20 18:13:44 +0530

    some more questions solved

--- lecture29.cpp
+++ lecture29.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
 
+    char str[10];
 
+    cout<<"enter char array which length u want: ";
+    cin.getline(str, 100);
+
+    for(char ch : str){
+        cout<< ch<< " ";
+    }
+    cout<<endl;
     return 0;
 }
